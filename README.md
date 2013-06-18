f73eebbd8b6e2879452643f34e853635a833b7e2
Aleksej Romanovskij <aleksij@gmail.com>
2013-06-18 18:57:28 +0300

    Initial commit - make zaebis'

---
+++ README.md
@@ -0,0 +1,30 @@
+# Starter
+
+A jQuery plugin to start another jQuery plugin.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/agat/jquery.starter/master/dist/starter.min.js
+[max]: https://raw.github.com/agat/jquery.starter/master/dist/starter.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/starter.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery.starter.js
@@ -0,0 +1,88 @@
+/*
+ * jquery.starter
+ * A jQuery plugin to start another jQuery plugin.
+ *
+ * Copyright (c) 2013 Aleksej Romanovskij
+ * Licensed under the MIT license.
+ */
+
+(function ($, undefined) {
+    'use strict';
+
+    // Plugin constructor
+    var Starter = function (elem, options) {
+            this.elem     = elem;
+            this.$elem    = $(elem);
+            this.options  = options;
+
+            this.metadata = this.$elem.data();
+
+            this._init();
+        },
+        namespace = 'starter';
+
+    // The plugin prototype
+    Starter.prototype = {
+        version     : 0.1,
+
+        // Plugin defaults options
+        defaults    : {
+            autoStart: true
+        },
+
+        /**
+         * Initialization procedures
+         * @private
+         */
+        _init: function () {
+            var starter = this,
+                plugin;
+
+            starter.config = $.extend(true, {}, starter.defaults, starter.options, starter.metadata);
+
+            plugin = starter.config[namespace];
+
+            if (plugin && plugin in $.fn) {
+                starter.$elem[plugin](starter.config);
+            } else {
+                starter._error("Please add jQuery plugin '%s' js-file to document.", plugin);
+            }
+
+            // console.dir(starter);
+        },
+
+        /**
+         * Show error message in console
+         * @private
+         */
+        _error: function () {
+            if ('console' in window) {
+                console.error.apply(console, arguments);
+            }
+
+            throw 42;
+        }
+    };
+
+    $.fn[namespace] = function (options) {
+        return this.each(function () {
+            var $this       = $(this),
+                plugin_here = namespace + ' here',
+                data        = $this.data(plugin_here);
+
+            if (!data) {
+                $this.data(plugin_here, new Starter(this, options));
+            }
+        });
+    };
+
+    $.fn[namespace].Constructor   = Starter;
+    $.fn[namespace].defaults      = Starter.prototype.defaults;
+
+    // Plugin initialization on DOM ready event
+    $(function () {
+        if ($.fn[namespace].defaults.autoStart) {
+            $('[data-' + namespace + ']')[namespace]();
+        }
+    });
+})(window.jQuery);
